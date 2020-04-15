1f0f1095ccd2efaaa62ec7b1f2831ef8391c2942
Souvik Biswas <sbis1999@gmail.com>
2020-04-15 23:19:29 +0530

    Initial main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,188 +3,109 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tipsy',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _textEditingController;
-  List<bool> _isSelected = [false, true, false];
-  List<double> _tipPercentageList = [0.10, 0.15, 0.20];
+  int _counter = 0;
 
-  double _bill;
-  double _tipAmount;
-  double _totalAmount;
-  double _tipPercentage;
-
-  @override
-  void initState() {
-    super.initState();
-    _bill = 0.0;
-    _tipAmount = 0.0;
-    _totalAmount = 0.0;
-    _tipPercentage = _tipPercentageList[1];
-
-    _textEditingController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _textEditingController?.dispose();
-    super.dispose();
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tipsy'),
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: Container(
-        color: Colors.tealAccent[100],
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              color: Colors.white,
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    'Enter bill total',
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        top: 30, bottom: 30, right: 20, left: 20),
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 40,
-                      ),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        icon: Icon(
-                          Icons.attach_money,
-                          size: 50,
-                        ),
-                        contentPadding: EdgeInsets.only(
-                            left: 15, bottom: 11, top: 11, right: 15),
-                        hintText: 'eg. 123.56',
-                      ),
-                      onChanged: (newAmount) => setState(() {
-                        _bill = double.parse(newAmount);
-                        _tipAmount = _bill * _tipPercentage;
-                        _totalAmount = (_bill + _tipAmount);
-                      }),
-                      controller: _textEditingController,
-                    ),
-                  ),
-                ],
-              ),
+            Text(
+              'You have pushed the button this many times:',
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 20),
-                      child: Text(
-                        'Select tip',
-                        style: TextStyle(
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                    ToggleButtons(
-                      fillColor: Colors.teal[700],
-                      selectedColor: Colors.white,
-                      textStyle: TextStyle(
-                        fontSize: 25,
-                      ),
-                      constraints: BoxConstraints(minWidth: 100, minHeight: 40),
-                      children: <Widget>[
-                        Text(
-                          '10%',
-                        ),
-                        Text(
-                          '15%',
-                        ),
-                        Text(
-                          '20%',
-                        ),
-                      ],
-                      isSelected: _isSelected,
-                      onPressed: (int index) {
-                        setState(() {
-                          _isSelected = [false, false, false];
-                          _isSelected[index] = !_isSelected[index];
-                          _tipPercentage = _tipPercentageList[index];
-                          _tipAmount = _bill * _tipPercentage;
-                          _totalAmount = (_bill + _tipAmount);
-                        });
-                      },
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30, bottom: 20),
-                      child: Text(
-                        'Tip Amount',
-                        style: TextStyle(
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      _tipAmount.toStringAsFixed(2),
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.teal[700],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30, bottom: 20),
-                      child: Text(
-                        'Total Amount',
-                        style: TextStyle(
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      _totalAmount.toStringAsFixed(2),
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.teal[700],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
